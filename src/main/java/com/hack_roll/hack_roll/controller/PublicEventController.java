f79b79646c10c2203f7c81939289e84b002d7344
Mariuxi <catmaluci@hotmail.com>
2025-05-01 15:18:38 +0200

    Get all events for public and use filters and pagination

---
+++ src/main/java/com/hack_roll/hack_roll/controller/PublicEventController.java
@@ -0,0 +1,53 @@
+package com.hack_roll.hack_roll.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.hack_roll.hack_roll.dto.EventFilterRequest;
+import com.hack_roll.hack_roll.dto.EventSpecifications;
+import com.hack_roll.hack_roll.model.Event;
+import com.hack_roll.hack_roll.service.EventService;
+
+
+@RestController
+@RequestMapping("/api/event")
+public class PublicEventController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    EventService eventService;
+   
+    @GetMapping("")
+    public ResponseEntity<Page<Event>> getAllEvents(@ModelAttribute EventFilterRequest filterRequest) {
+        Specification<Event> filters = Specification.where(null);
+
+        if (filterRequest.getTitle() != null && !filterRequest.getTitle().isEmpty()) {
+            filters = filters.and(EventSpecifications.hasTitle(filterRequest.getTitle()));
+        }
+        if (filterRequest.getCategory() != null && !filterRequest.getCategory().isEmpty()) {
+            filters = filters.and(EventSpecifications.hasCategory(filterRequest.getCategory()));
+        }
+        if (filterRequest.getDate() != null) {
+            filters = filters.and(EventSpecifications.hasDate(filterRequest.getDate()));
+        }
+        if (filterRequest.getCreatedBy() != null && !filterRequest.getCreatedBy().isEmpty()) {
+            filters = filters.and(EventSpecifications.createdByUserName(filterRequest.getCreatedBy()));
+        }
+
+        Pageable paging = PageRequest.of(filterRequest.getPage(), filterRequest.getSize());
+
+        Page<Event> events = eventService.getAllEvents(filters, paging);
+
+        return ResponseEntity.ok(events);
+    }
+}
